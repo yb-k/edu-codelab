6fee6cc0d4b6454775ee22a210a6899fd5d820ef
yb-k <92908261+yb-k@users.noreply.github.com>
2023-07-04 08:51:59 +0900

    remove: delete bilerplate directory
    seperate  repository
    https://github.com/yb-k/simple-json-server
    https://github.com/yb-k/vue2-boilerplate
    https://github.com/yb-k/nuxt3-boilerplate

--- 05_BOILERPLATE/RESTAPI_SERVER/dbJson.js
+++
@@ -1,4 +0,0 @@
-const path = require("path");
-const dbJson = path.resolve(__dirname, "db.json");
-
-module.exports = dbJson;
--- 05_BOILERPLATE/RESTAPI_SERVER/middlewares/authenticationRouter.js
+++
@@ -1,9 +0,0 @@
-require("../env");
-const { PORT, AUTHORIZATION, CORS } = process.env;
-
-const authRouter = {
-  session: require("./authSESSION"),
-  jwt: require("./authJWT"),
-};
-
-module.exports = authRouter[AUTHORIZATION];
